83c91f6d3a20131f4d61b727ae17d0f639997d0c
akshay.ts <akshay.ts@zohocorp.com>
2021-06-26 00:31:49 +0530

    UI test case added for Add Task action

--- MyToDoListAppUITests/MyToDoListAppUITests.swift
+++ MyToDoListAppUITests/MyToDoListAppUITests.swift
@@ -9,34 +9,28 @@ import XCTest
 
 class MyToDoListAppUITests: XCTestCase {
 
+	private var app: XCUIApplication!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+		self.app = XCUIApplication()
+		self.app.launch()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testAddingNewTask() throws {
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+		let addButton = self.app.buttons["addNewTask"]
+		let newTaskTextField = self.app.textFields["taskTextField"]
+		newTaskTextField.tap()
+		newTaskTextField.typeText("Do laundry.")
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+		addButton.tap()
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+		let list = self.app.tables["contentList"]
+		let cells = list.cells
+		XCTAssertEqual(cells.count, 1)
+
+//		let checkBoxButton = cells["completedCheckBoxButton"]
+//		checkBoxButton.
     }
 }
